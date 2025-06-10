40c3a6e0c01ae07208228197ea5c8c052fdb4743
Joshua Uzoagulu <joshua.uzoagulu@netapps.com.ng>
2025-06-11 00:07:10 +0100

    Add Transaction entity  sqlite config

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,10 +1,11 @@
 import { DataSource } from 'typeorm';
 import { User } from './src/user/user.entity';
 import { Wallet } from './src/wallet/wallet.entity';
+import { Transaction } from 'src/transaction/transaction.entity';
 
 export default new DataSource({
   type: 'sqlite',
   database: 'db.sqlite',
-  entities: [User, Wallet],
+  entities: [User, Wallet, Transaction],
   synchronize: true,
 });
